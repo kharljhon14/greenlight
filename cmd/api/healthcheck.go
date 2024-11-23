f2d1ca90af646384fffa608519987a1c6d33ae66
Kharl <enriquezkharl14@gmail.com>
2024-11-23 15:19:23 +0800

    [Feat] Update healthcheck route to return JSON

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -6,7 +6,12 @@ import (
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "enviroment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	// Fixed json response
+	js := `{"status":"available", "enviroment": %q, "version": %q}`
+	js = fmt.Sprintf(js, app.config.env, version)
+
+	// Set Content-Type to application/json on the header
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Write([]byte(js))
 }
