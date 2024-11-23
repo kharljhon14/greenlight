39e29f2717ad04be3d480b50a097d45d515e2fa6
Kharl <enriquezkharl14@gmail.com>
2024-11-23 16:03:15 +0800

    [Refactor] Update healthcheck to use json marshal

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,17 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	// Fixed json response
-	js := `{"status":"available", "enviroment": %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
+	// Map
+	data := map[string]string{
+		"status":     "available",
+		"enviroment": app.config.env,
+		"version":    version,
+	}
 
-	// Set Content-Type to application/json on the header
+	// Pass  the json map to the json.marshal
+	js, err := json.Marshal(data)
+	if err != nil {
+		// Send generic message
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
+
+	// Append newline to the json
+	js = append(js, '\n')
+
 	w.Header().Set("Content-Type", "application/json")
 
-	w.Write([]byte(js))
+	w.Write(js)
 }
