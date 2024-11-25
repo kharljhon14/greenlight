397f49041a2dc2e307e35cb3baa8bd2c530e4c21
Kharl <enriquezkharl14@gmail.com>
2024-11-25 12:39:18 +0800

    [Feat] Adde error response helpers

---
+++ cmd/api/errors.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) logError(r *http.Request, err error) {
+	app.logger.Println(err)
+}
+
+// Generic error Response  for sending json formatted error messages
+func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message interface{}) {
+	env := envelope{"error": message}
+
+	err := app.writeJSON(w, status, env, nil)
+	// Return a 500 internal server error if there is a problem parsing the json
+	if err != nil {
+		app.logError(r, err)
+		w.WriteHeader(500)
+	}
+}
+
+func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.logError(r, err)
+
+	message := "the server encountered a problem and could not process your request"
+	app.errorResponse(w, r, http.StatusInternalServerError, message)
+}
+
+func (app *application) notFoundErrorResponse(w http.ResponseWriter, r *http.Request) {
+	message := "the request resource could not be found"
+	app.errorResponse(w, r, http.StatusNotFound, message)
+}
+
+func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
+	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
+	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
+}
