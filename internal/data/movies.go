68214fe22048dd4b546e19fec9b34ce995010202
Kharl <enriquezkharl14@gmail.com>
2024-12-10 03:33:22 +0800

    [Feat] Prevent race conditions

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -11,6 +11,7 @@ import (
 
 var (
 	ErrRecordNotFound = errors.New("record not found")
+	ErrEditConflict   = errors.New("edit conflict")
 )
 
 type Movie struct {
@@ -99,7 +100,7 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 func (m MovieModel) Update(movie *Movie) error {
 
 	query := `UPDATE movies SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1 
-	WHERE id = $5 RETURNING version`
+	WHERE id = $5 AND version $6 RETURNING version`
 
 	// Slice containing the valurs for placeholder parameters
 	args := []interface{}{
@@ -108,10 +109,21 @@ func (m MovieModel) Update(movie *Movie) error {
 		movie.Runtime,
 		pq.Array(movie.Genres),
 		movie.ID,
+		movie.Version,
 	}
 
-	return m.DB.QueryRow(query, args...).Scan(&movie.Version)
+	// Check if the movie is currently being edited to prevent race conditions
+	err := m.DB.QueryRow(query, args...).Scan(&movie.Version)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
 
+	return nil
 }
 
 func (m MovieModel) Delete(id int64) error {
