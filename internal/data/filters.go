026d8168987f64f23710939a489dfbc76f50405e
Kharl <enriquezkharl14@gmail.com>
2024-12-18 02:04:31 +0800

    [Feat] Added validation of filters

---
+++ internal/data/filters.go
@@ -0,0 +1,20 @@
+package data
+
+import "github.com/kharljhon14/greenlight/internal/validator"
+
+type Filter struct {
+	Page         int
+	PageSize     int
+	Sort         string
+	SortSafeList []string
+}
+
+func ValidateFilters(v *validator.Validator, f Filter) {
+	v.Check(f.Page > 0, "page", "must be grater than zero")
+	v.Check(f.Page <= 10_000_000, "page", "must be a maximum of 10 million")
+	v.Check(f.PageSize > 0, "page_size", "must be greater than zero")
+	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
+
+	// Check that the sort parameter matches a value in the safelist
+	v.Check(validator.In(f.Sort, f.SortSafeList...), "sort", "invalid sort value")
+}
