303845a51462489d06715b4f8a8d1420aee00e52
Kharl <enriquezkharl14@gmail.com>
2024-12-18 09:57:10 +0800

    [Feat] Getting all movies with filter

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -110,14 +110,17 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 func (m MovieModel) GetAll(title string, genres []string, filters Filter) ([]*Movie, error) {
 
 	// Query
-	query := `SELECT id, created_at, title, year, runtime, genres, version 
-	FROM movies ORDER BY id`
+	query := `SELECT id, created_at, title, year, runtime, genres, version
+        FROM movies
+        WHERE (LOWER(title) = LOWER($1) OR $1 = '') 
+        AND (genres @> $2 OR $2 = '{}')     
+        ORDER BY id`
 
 	// Create context with 3 second timeout
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	rows, err := m.DB.QueryContext(ctx, query)
+	rows, err := m.DB.QueryContext(ctx, query, title, pq.Array(genres))
 	if err != nil {
 		return nil, err
 	}
