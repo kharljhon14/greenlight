168242a3e68403e800ea2e4d8e87b203ba6ced2d
Kharl <enriquezkharl14@gmail.com>
2024-12-20 03:01:39 +0800

    [Feat] Panic Recovery

---
+++ cmd/api/middleware.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Deferred function (which will always be run in the event of a panic)
+		defer func() {
+
+			if err := recover(); err != nil {
+				// Set "Connection: close"  header on the response
+				w.Header().Set("Connection", "close")
+
+				app.serverErrorResponse(w, r, fmt.Errorf("%s", err))
+			}
+		}()
+
+		next.ServeHTTP(w, r)
+	})
+}
