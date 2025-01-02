6fbc3bb6d8c95265d31f97be162b2260f433da73
Kharl <enriquezkharl14@gmail.com>
2025-01-03 00:16:12 +0800

    [Feat] Sending activation token

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"net/http"
+	"time"
 
 	"github.com/kharljhon14/greenlight/internal/data"
 	"github.com/kharljhon14/greenlight/internal/validator"
@@ -54,8 +55,20 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
+	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
 	app.background(func() {
-		err = app.mailer.Send(user.Email, "user_welcome.tmpl.html", user)
+
+		data := map[string]interface{}{
+			"activationToken": token.Plaintext,
+			"userID":          user.ID,
+		}
+
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl.html", data)
 		if err != nil {
 			app.logger.PrintError(err, nil)
 		}
--- internal/data/tokens.go
+++ internal/data/tokens.go
@@ -75,8 +75,8 @@ func (m TokenModel) New(userID int64, ttl time.Duration, scope string) (*Token, 
 }
 
 func (m TokenModel) Insert(token *Token) error {
-	query := `INSERT INTO tokens (hash, user_id, expirty, scope)
-	VALUES ($1, $2, $3)`
+	query := `INSERT INTO tokens (hash, user_id, expiry, scope)
+	VALUES ($1, $2, $3, $4)`
 
 	args := []interface{}{token.Hash, token.UserID, token.Expiry, token.Scope}
 
